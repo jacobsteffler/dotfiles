1c754ebe7c0a4be348761bef717b9e5c5f103646
Jacob Steffler <jsteffle@epic.com>
2024-11-05 15:20:14 -0600

    Just disable cursor animation in neovide

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,9 +108,7 @@ vim.o.shiftwidth = 4
 
 if vim.g.neovide then
     vim.g.neovide_scroll_animation_length = 0.2
-    vim.g.neovide_cursor_animate_command_line = false
-    vim.g.neovide_cursor_animation_length = 0.03
-    vim.g.neovide_cursor_trail_size = 0.3
+    vim.g.neovide_cursor_animation_length = 0
 end
 
 -- Load vimrc configuration
